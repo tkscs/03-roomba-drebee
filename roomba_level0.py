79d30f240332966c567ba9985b3011413113d055
drebee <ebennett@kehillah.org>
2025-09-04 09:22:24 -0700

    roomba moves

--- roomba_level0.py
+++ roomba_level0.py
@@ -5,7 +5,7 @@
 # Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
  
-from turtle import right, left, forward, backward
+from turtle import right, left, forward, backward, speed
 import room
 
 # Draw the Level 0 version of the room
@@ -13,8 +13,10 @@ window = room.draw_room(level = 0)
 
 ###
 # Start your code here
- 
- 
+speed(1)
+
+right(90)
+forward(40)
  
 # End your code here
 ###
